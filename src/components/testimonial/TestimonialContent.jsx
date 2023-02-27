6dd913c003adebbbf182cfb277e445905e7249bd
jCasia <casjay70@gmail.com>
2023-02-27 16:41:33 +0000

    minor img update

--- src/components/testimonial/TestimonialContent.jsx
+++ src/components/testimonial/TestimonialContent.jsx
@@ -5,7 +5,7 @@ const TestimonialContent = (props) => {
   return (
     <div className="testimonial-container">
       <div className="img-name-wrapper">
-        <img src={props.img} className="testimonial-img" alt="" />
+        <img src={props.img} className="testimonial-img" alt="profile img" />
         <div className="name-buyer-wrapper">
           <p className="testimonial-name">{props.name}</p>
           <p className="testimonial-status">{props.buyer}</p>
